9a61f76c92ac02ef677e009556ca643ab5111beb
vehbioztomurcuk <vehbi@oztomurcuk.com>
2023-02-09 12:34:23 +0300

    hide

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTA3MzA3Mjg4OTM0MDg5OTM4OA.Gc2rbC.pUAbSHwUQDVLJgeJudNiBOCjd0YFUtOWqFaMvc'
+    TOKEN = '<your_token_here>'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
